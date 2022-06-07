b6c77ef53a20c2da90f0a1187feccc41a84311b8
Arne Ludwig <arne.ludwig@posteo.de>
2022-06-07 17:38:24 +0200

    Visualize dataset

--- cnn-elpv-dataset.py
+++ cnn-elpv-dataset.py
@@ -5,6 +5,33 @@
 @copyright: Copyright © 2022 by Arne Ludwig
 """
 
+import matplotlib.pyplot as plt
+
 from elpv_dataset.utils.elpv_reader import load_dataset
 
+# %% Load data
+
 images, probs, types = load_dataset()
+
+
+# %% Visualize data
+
+n_samples = 3
+plt.figure(figsize=(6.4, n_samples * 3.2))
+k = 0
+for i, type_ in enumerate(["mono"] * n_samples + ["poly"] * n_samples):
+    for j in range(4):
+        prob = j / 3
+        sample = images[(probs == prob) & (types == type_)][i % n_samples]
+        k += 1
+        plt.subplot(2 * n_samples, 4, k)
+        plt.imshow(sample)
+        plt.axis("image")
+        plt.xticks([])
+        plt.yticks([])
+        if i == 0:
+            plt.title(f"p = {j}/3")
+        if j == 0 and i % n_samples == 0:
+            plt.ylabel(f"← {type_}")
+plt.tight_layout()
+plt.show()
