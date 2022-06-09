746f02158d9a516447e59ef427ae96f5aaeb103a
Arne Ludwig <arne.ludwig@posteo.de>
2022-06-09 14:43:51 +0200

    Beautified evaluation plot for probs

--- cnn-elpv-dataset.py
+++ cnn-elpv-dataset.py
@@ -392,9 +392,9 @@ if not training_mode:
     plt.show()
 
     plt.figure(figsize=(6.4, 6.4))
-    sns.regplot(y_probs_test, test_pred[1])
+    sns.violinplot(y_probs_test.reshape(-1), test_pred[1].reshape(-1), inner="stick")
     plt.title("Defect Probability")
-    plt.xlim([0, 1])
+    plt.xticks(range(4), ["0", "⅓", "⅔", "1"])
     plt.xlabel("Test Values")
     plt.ylim([0, 1])
     plt.ylabel("Predicted Values")
