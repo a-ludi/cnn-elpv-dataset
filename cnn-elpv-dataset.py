860dc71da2460cfe7082b9a18a9ec75cb2e33527
Arne Ludwig <arne.ludwig@posteo.de>
2022-06-09 13:44:20 +0200

    Architecture v2 and more
    - implemented architecture v2 with dedicated dense layers
      for each cell type
    - account for non-uniformly distributed defect probs.
    - more evaluation procedures

--- cnn-elpv-dataset.py
+++ cnn-elpv-dataset.py
@@ -7,11 +7,12 @@
 
 import json
 import os
+import time
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 
@@ -19,8 +20,20 @@ from elpv_dataset.utils.elpv_reader import load_dataset
 
 # %% Initialization
 
+use_pretrained_type_model = True
+
 models_root = Path("./models")
 models_root.mkdir(parents=True, exist_ok=True)
+
+history_path = models_root / "history.json"
+metrics_path = models_root / "metrics.json"
+model_path = models_root / "model.h5"
+type_history_path = models_root / "type_history.json"
+type_model_path = models_root / "type_model.h5"
+
+# Only use pre-trained type model if it exists!
+if not type_model_path.exists():
+    use_pretrained_type_model = False
 
 # Detect whether we are running on a cluster for training only
 training_mode = "SLURM_JOB_ID" in os.environ
@@ -32,6 +45,12 @@ if "SLURM_CPUS_PER_TASK" in os.environ:
     tf.config.threading.set_intra_op_parallelism_threads(cpus)
     tf.config.threading.set_inter_op_parallelism_threads(cpus)
 
+# Avoid importing plotting libraries in training mode because they usually
+# takes a couple of seconds to load
+if not training_mode:
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
 
 # %% Load data
 
@@ -41,8 +60,8 @@ images, probs, types = load_dataset()
 # %% Inspect data
 
 pixel_range = np.amin(images), np.amax(images)
-prob_values = np.unique(probs)
-type_values = np.unique(types)
+prob_values, probs_counts = np.unique(probs, return_counts=True)
+type_values, type_counts = np.unique(types, return_counts=True)
 
 if not training_mode:
     print("Dataset Characteristics")
@@ -80,6 +99,14 @@ if not training_mode:
                 plt.title(f"p = {j}/3")
             if j == 0 and i % n_samples == 0:
                 plt.ylabel(f"← {type_}")
+    plt.tight_layout()
+    plt.show()
+
+    plt.hist(probs)
+    plt.tight_layout()
+    plt.show()
+
+    plt.hist(types)
     plt.tight_layout()
     plt.show()
 
@@ -149,6 +176,23 @@ def mean_absolute_error(y_true, y_pred):
     return np.mean(np.abs(y_true - y_pred), axis=0)
 
 
+def r2_score(y_true, y_pred):
+    """Compute coefficient of determination, denoted R².
+
+    From Wikipedia:
+        In statistics, the coefficient of determination, denoted R² or r² and
+        pronounced "R squared", is the proportion of the variation in the
+        dependent variable that is predictable from the independent
+        variable(s).
+
+    @returns  Coefficients of determination of shape `y_true.shape[1:]`
+    """
+    y_diff = tf.reduce_sum(tf.square(y_true - y_pred), axis=0)
+    y_square = tf.reduce_sum(tf.square(y_true - tf.reduce_mean(y_true)), axis=0)
+
+    return 1 - y_diff / y_square
+
+
 cnn = keras.applications.MobileNetV3Large(
     input_shape=X.shape[1:],
     alpha=1.0,
@@ -173,18 +217,26 @@ type_model.compile(
     metrics="binary_accuracy",
 )
 
-features_type = keras.layers.Concatenate()([flat_feature_maps, type_])
-prob = keras.layers.Dense(1024, activation="sigmoid", name="dense_prob_1")(
-    features_type
+prob1 = keras.layers.Dense(1, activation="sigmoid", name="dense_prob_1")(
+    flat_feature_maps
 )
-prob = keras.layers.Dense(1, activation="sigmoid", name="dense_prob_2")(prob)
-model = keras.Model(inputs, [type_, prob], name="elpv")
+modulated_prob1 = keras.layers.Multiply()([prob1, type_])
+prob2 = keras.layers.Dense(1, activation="sigmoid", name="dense_prob_2")(
+    flat_feature_maps
+)
+type_inv = keras.layers.Lambda(lambda x: 1 - x, name="invert_type")(type_)
+modulated_prob2 = keras.layers.Multiply()([prob2, type_inv])
 
+combined_prob = keras.layers.Concatenate()([modulated_prob1, modulated_prob2])
+prob = keras.layers.Dense(1, activation="sigmoid", name="dense_prob_out")(combined_prob)
+
+model = keras.Model(inputs, [type_, prob], name="elpv")
 
 model.compile(
     loss=["binary_crossentropy", "MSE"],
-    optimizer=keras.optimizers.RMSprop(learning_rate=1e-2),
-    metrics=[["binary_accuracy"], ["MAE"]],
+    loss_weights=[0.5, 1.0],
+    optimizer=keras.optimizers.RMSprop(learning_rate=1e-4),
+    metrics=[["binary_accuracy"], ["MAE", r2_score]],
 )
 model.summary()
 
@@ -192,33 +244,61 @@ model.summary()
 # %% Training
 
 if training_mode:
-    # 1. Train cell type model only
-    type_model.fit(
-        X_train,
-        y_types_train,
-        epochs=15,
-        validation_data=(X_test, y_types_test),
-        batch_size=8,
-    )
-    type_model.save(models_root / "type_model.h5", save_format="h5")
-    type_history = dict(type_model.history.history)
-    with (models_root / "type_history.json").open("w") as outfile:
-        json.dump(type_history, outfile)
+    training_begin = time.strftime("%Y-%m-%dT%H-%M-%S")
+
+    if use_pretrained_type_model:
+        type_model = keras.models.load_model(type_model_path)
+    else:
+        # 1. Train cell type model only
+        type_model.fit(
+            X_train,
+            y_types_train,
+            epochs=15,
+            validation_data=(X_test, y_types_test),
+            batch_size=8,
+            callbacks=[
+                keras.callbacks.EarlyStopping(
+                    monitor="val_binary_accuracy",
+                    min_delta=1e-5,
+                    patience=2,
+                    verbose=1,
+                    restore_best_weights=True,
+                ),
+            ],
+        )
+        type_model.save(type_model_path, save_format="h5")
+        type_history = dict(type_model.history.history)
+        type_history["__timestamp__"] = training_begin
+        with type_history_path.open("w") as outfile:
+            json.dump(type_history, outfile)
 
     # Freeze weights of type model
-    type_model.trainable = False
+    type_model.trainable = True
+    cnn.trainable = True
 
     # 2. Train defect probability model only
+
+    # Compensate non-uniform distribution of probs
+    y_probs_train_values, y_probs_train_counts = np.unique(
+        y_probs_train, return_counts=True
+    )
+    probs_weights = y_probs_train_counts.sum() / y_probs_train_counts
+    sample_weights = np.zeros(y_probs_train.shape, dtype=float)
+    for prob, weight in zip(y_probs_train_values, probs_weights):
+        sample_weights[y_probs_train == prob] = weight
+
     model.fit(
         X_train,
         [y_types_train, y_probs_train],
-        epochs=15,
+        epochs=200,
+        sample_weight=sample_weights,
         validation_data=(X_test, (y_types_test, y_probs_test)),
         batch_size=8,
     )
-    model.save(models_root / "model.h5", save_format="h5")
+    model.save(model_path, save_format="h5")
     history = dict(model.history.history)
-    with (models_root / "history.json").open("w") as outfile:
+    history["__timestamp__"] = training_begin
+    with history_path.open("w") as outfile:
         json.dump(history, outfile)
 
     train_pred = model.predict(X_train)
@@ -230,60 +310,120 @@ if training_mode:
         test_type_accuracy=binary_accuracy(y_types_test, test_pred[0]).item(),
         test_prob_mae=mean_absolute_error(y_probs_test, test_pred[1]).item(),
     )
-    with (models_root / "metrics.json").open("w") as outfile:
+    metrics["__timestamp__"] = training_begin
+    with metrics_path.open("w") as outfile:
         json.dump(metrics, outfile)
 
 
 # %% Evaluation
 
+
+def plot_learning_curve(history, name, exclude=[]):
+    """Plot learning curve(s) from history dictionary."""
+    for key, curve in history.items():
+        if any(ex in key for ex in exclude):
+            continue
+
+        if key.endswith("loss"):
+            ls = "-."
+        elif key.startswith("val"):
+            ls = "-"
+        else:
+            ls = ":"
+        plt.plot(curve, ls=ls, label=key)
+    plt.grid(True)
+    # plt.legend(loc=1)
+    plt.title(f"Learning Curve of {name}")
+    plt.xlabel("Epoch")
+    plt.show()
+
+
 if not training_mode:
-    model = keras.models.load_model(models_root / "model.h5")
-    with (models_root / "type_history.json").open() as infile:
-        type_history = json.load(infile)
-    with (models_root / "history.json").open() as infile:
+    if not use_pretrained_type_model:
+        type_model = keras.models.load_model(type_model_path)
+        with (type_history_path).open() as infile:
+            type_history = json.load(infile)
+            print(
+                "-- loaded `type_history` for run "
+                + type_history.pop("__timestamp__", "N/A")
+            )
+
+        plot_learning_curve(type_history, "Type Model")
+
+        test_pred_type = type_model.predict(X_test)
+
+        cm = confusion_matrix(y_types_test, test_pred_type >= 0.5)
+        disp = ConfusionMatrixDisplay(cm, display_labels=type_values)
+        disp.plot()
+
+    model = keras.models.load_model(model_path, custom_objects=dict(r2_score=r2_score))
+    with (history_path).open() as infile:
         history = json.load(infile)
-    with (models_root / "metrics.json").open() as infile:
+        print("-- loaded `history` for run " + history.pop("__timestamp__", "N/A"))
+    with (metrics_path).open() as infile:
         metrics = json.load(infile)
+        print("-- loaded `metrics` for run " + metrics.pop("__timestamp__", "N/A"))
 
     print("Final metrics:")
     for key, val in metrics.items():
         print(f"  {key:<20s}: {val:9.2f}")
 
-    for key, hist in type_history.items():
-        if key.startswith("val"):
-            if key.endswith("loss"):
-                ls = "--"
-            else:
-                ls = "-"
-        else:
-            if key.endswith("loss"):
-                ls = "-."
-            else:
-                ls = ":"
-        plt.plot(hist, ls=ls, label=key)
-    plt.grid(True)
-    plt.legend(loc=1)
-    plt.title("Learning Curve of Type Model")
-    plt.xlabel("Epoch")
+    plot_learning_curve(
+        dict(
+            (key, history[key])
+            for key in ["dense_prob_out_loss", "val_dense_prob_out_loss"]
+        ),
+        "Defect Prob. Model",
+    )
+    plot_learning_curve(
+        dict(
+            (key, history[key])
+            for key in ["dense_prob_out_r2_score", "val_dense_prob_out_r2_score"]
+        ),
+        "Defect Prob. Model",
+    )
+    plot_learning_curve(history, "Defect Prob. Model", exclude=["dense_type_1", "MAE"])
+
+    test_pred = model.predict(X_test)
+
+    cm = confusion_matrix(y_types_test, test_pred[0] >= 0.5)
+    disp = ConfusionMatrixDisplay(cm, display_labels=type_values)
+    disp.plot()
     plt.show()
 
-    for key, hist in history.items():
-        if "dense_type_1" in key:
-            continue
+    plt.figure(figsize=(6.4, 6.4))
+    sns.regplot(y_probs_test, test_pred[1])
+    plt.title("Defect Probability")
+    plt.xlim([0, 1])
+    plt.xlabel("Test Values")
+    plt.ylim([0, 1])
+    plt.ylabel("Predicted Values")
+    plt.show()
 
-        if key.startswith("val"):
-            if key.endswith("loss"):
-                ls = "--"
-            else:
-                ls = "-"
-        else:
-            if key.endswith("loss"):
-                ls = "-."
-            else:
-                ls = ":"
-        plt.plot(hist, ls=ls, label=key)
-    plt.grid(True)
-    plt.legend(loc=1)
-    plt.title("Learning Curve of Full Model")
-    plt.xlabel("Epoch")
+    prob_test_pred_quant = np.round(3 * test_pred[1]) / 3
+    plt.figure(figsize=(6.4, 6.4))
+    for i, pred_prob in enumerate(prob_values):
+        mask_pred = prob_test_pred_quant == pred_prob
+        for j, true_prob in enumerate(prob_values):
+            mask = (y_probs_test == true_prob) & mask_pred
+            mask = mask.reshape(-1)
+            sample = X_test[mask][:, :, :, 0]
+
+            for k in range(4):
+                k1, k2 = divmod(k, 2)
+                row = 2 * i + k1
+                col = 2 * j + k2
+                plt.subplot(8, 8, 8 * row + col + 1)
+
+                if k < len(sample):
+                    plt.imshow(sample[k])
+
+                plt.axis("image")
+                plt.xticks([])
+                plt.yticks([])
+                if row == 0 and k == 0:
+                    plt.title(f"true={true_prob:.1f}")
+                if col == 0 and k == 0:
+                    plt.ylabel(f"pred={pred_prob:.1f}")
+    plt.tight_layout()
     plt.show()
